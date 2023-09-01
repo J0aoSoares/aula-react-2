645b7fa6fbbc5a84d553117dc251531e3427804c
joao <joaorodrigues.009@outlook.com>
2023-09-01 20:51:45 -0300

    finalizado

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import {} from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Nav from './Nav'
+import Exemplo1 from './Exemplo1'
+import Exemplo2 from './Exemplo2'
+import Exemplo3 from './Exemplo3'
+import Footer from './Footer'
+
+function App() {
+
+
+  return (
+    <>
+
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path="/Exemplo1" element={<Exemplo1/>}/>
+        <Route path="/Exemplo2" element={<Exemplo2/>}/>
+        <Route path="/Exemplo3" element={<Exemplo3/>}/>
+
+      </Routes>
+    <Footer/>
+    
+     
+    </BrowserRouter>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Exemplo2.jsx
@@ -0,0 +1,13 @@
+import {} from 'react'
+
+function Exemplo2(){
+
+    return(
+        <>
+        <h1>Exemplo2</h1>
+        </>
+    )
+
+
+}
+export default Exemplo2
